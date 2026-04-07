870122851643428fdc623d782e5166b619d21076
cm25acq <266096280+cm25acq@users.noreply.github.com>
2026-04-07 18:06:00 +0100

    Minor fixes
    --Added styling to error.php
    --Changed admin nav links in template_navbar.php

--- CultureConnect/templates/template_navbar.php
+++ CultureConnect/templates/template_navbar.php
@@ -38,9 +38,9 @@
                     } else if ($_SESSION['role'] == 4) {
                         echo "  <a id='home' class='nav-link d-inline' href='00Home.php'>Home</a>
                                 <a id='edit_profile' class='nav-link d-inline' href='02ManageUser.php'>Edit profile</a>
-                                <a id='edit_busines' class='nav-link d-inline' href='03EditBusiness.php'>Edit business deatils</a>
-                                <a id='edit_busines' class='nav-link d-inline' href='03EditBusiness.php'>Edit council details</a>
-                                <a id='manage_offerings'class='nav-link d-inline' href='04ManageOfferings.php'>Manage offerings</a>
+                                <a id='edit_busines' class='nav-link d-inline' href='97ManageBusinessAdmin.php'>Edit business details</a>
+                                <a id='edit_busines' class='nav-link d-inline' href='97ManageCouncilAdmin.php'>Edit council details</a>
+                                <a id='manage_offerings'class='nav-link d-inline' href='97ManageOfferingsAdmin.php'>Manage offerings</a>
                                 <a id='log_out' class='nav-link d-inline' href='include/logout.php'>Log out</a>";
 
                     } else {
