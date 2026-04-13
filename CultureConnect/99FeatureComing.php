ec387504313e57524010bcb80d81dec9a5212c6f
cm25acq <266096280+cm25acq@users.noreply.github.com>
2026-04-13 19:08:12 +0100

    Feature coming page
    -- added header and footer to Feature coming, and new text
    --Updated references

--- CultureConnect/99FeatureComing.php
+++ CultureConnect/99FeatureComing.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site map</title>
+    <title>Feature coming</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
@@ -13,40 +13,17 @@
 <body>
     <?php
     session_start();
-    include('include/config.php')
+    include('include/config.php');
         ?>
     <!-- Gets the header from a central location -->
     <div id="header"><?php include('templates/template_navbar.php'); ?></div>
     <!-- Header-->
     <section class="text-left py-5" style="background-color:#ACC8A2;">
-        <h1 style="margin-left: 150px;">Site map</h1>
+        <h1 style="margin-left: 150px;">Feature coming</h1>
     </section>
     <!-- Main content -->
     <section class="text-left py-5" style="margin-left: 150px;">
-        <ul>
-            <a href="00Home.php">Home</a>
-        </ul>
-        <ul>
-            <a href="02RegisterUser.php">Register</a>
-        </ul>
-        <ul>
-            <a href="03EditBusiness.php">Edit business</a>
-        </ul>
-        <ul>
-            <a href="04ManageOfferings.html">Manage offerings</a>
-        </ul>
-        <ul>
-            <a href="05EditOffering.php">Offering details</a>
-        </ul>
-        <ul>
-            <a href="01EditLocations.php">Manage locations</a>
-        </ul>
-        <ul>
-            <a href="99FeatureComing.php">Edit user profile</a>
-        </ul>
-        <ul>
-            <a href="99FeatureComing.php">Admin pages</a>
-        </ul>
+        <p>This is a test website. Some features are not yet implemented.</p>
     </section>
     <!-- Gets the footer from a central location -->
     <div id="footer"><?php include('templates/template_footer.php'); ?></div>
